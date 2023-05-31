5177326a4d460ee9516c1bbaeb39ab2c46bdcc46
ANSHUL RAWAT <anshulrawat@192.168.1.8>
2023-06-01 00:16:43 +0530

    add dark and light mode feature

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// $(".theme-switch").on("click", () => {
-//     $("body").toggleClass("light-theme");
-//   });
 
 // burgerlinks
-
-
 const burgerLinks = document.querySelector('.burger-links');
 const burger = document.querySelector('.burger');
 const body = document.querySelector('body');
@@ -16,6 +11,7 @@ toggleButton = () =>
   body.classList.toggle('active');
 }
 
+
 // const container = document.querySelector('.burger');
 // const bars = document.querySelectorAll('.bar');
 // const body = document.querySelector('body'); 
@@ -26,43 +22,16 @@ toggleButton = () =>
 // });
 
 
-// Logic for Section visting 
 
-// const navigationHeight = document.querySelector('.nav-wrapper').offsetHeight;
-// console.log(navigationHeight); 
-// document.documentElement.style.setProperty("--scroll-padding", navigationHeight + "px"); 
+ // Function to update the navbar color based on the current theme
+// Function to update the navbar color based on the current theme
 
+let darkColor = '#323232'; 
+let lightColor = "#fdf6f8"; 
 
-// Navbar Color change while scrolling
-
-// // Select the navbar element
-// const navbar = document.querySelector('.nav-wrapper');
-
-// // Set the desired color for the navbar
-// const navbarColor = '#fff';
-
-// // Function to handle scroll event
-// function handleScroll() {
-//   // Get the vertical scroll position
-//   const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-//   // Check if the scroll position is greater than a certain value (e.g., 100 pixels)
-//   console.log(scrollPosition);
-//   if (scrollPosition > 700) {
-//     // Change the navbar color
-//     navbar.style.backgroundColor = navbarColor;
-//   }
-//    else  {
-//     // Reset the navbar color
-//     navbar.style.backgroundColor = '#fdf6f8';
-//   }
-// }
-
-// // Add scroll event listener
-// window.addEventListener('scroll', handleScroll);
-
-window.addEventListener('scroll', function() {
+function updateNavbarColor() {
   const navbar = document.querySelector('.nav-wrapper');
+  const introSection = document.querySelector('.intro-section');
   const section1 = document.querySelector('.project-section');
   const section2 = document.querySelector('.about-section');
   const section3 = document.querySelector('.footer');
@@ -71,16 +40,114 @@ window.addEventListener('scroll', function() {
   const section2Offset = section2.offsetTop;
   const section3Offset = section3.offsetTop;
 
-
   const scrollPosition = window.pageYOffset + window.innerHeight / 8;
-  if (scrollPosition > section1Offset && scrollPosition < section2Offset) {
-    navbar.style.backgroundColor = '#fff';
-  } else if (scrollPosition > section2Offset && scrollPosition < section3Offset) {
-    navbar.style.backgroundColor = '#fdf6f8';
+
+  const theme = document.documentElement.getAttribute('data-theme');
+  const fallbackColor = theme === 'dark' ? darkColor : lightColor;
+
+  if (scrollPosition >= section1Offset && scrollPosition < section2Offset) {
+    const section1Color = getComputedStyle(section1).backgroundColor;
+    navbar.style.backgroundColor = section1Color !== 'rgba(0, 0, 0, 0)' ? section1Color : fallbackColor;
+  } else if (scrollPosition >= section2Offset && scrollPosition < section3Offset) {
+    const section2Color = getComputedStyle(section2).backgroundColor;
+    navbar.style.backgroundColor = section2Color !== 'rgba(0, 0, 0, 0)' ? section2Color : fallbackColor;
+  } else if (scrollPosition >= section3Offset) {
+    const section3Color = getComputedStyle(section3).backgroundColor;
+    navbar.style.backgroundColor = section3Color !== 'rgba(0, 0, 0, 0)' ? section3Color : fallbackColor;
   } else {
-    navbar.style.backgroundColor = '#fdf6f8';
+    const introSectionColor = getComputedStyle(introSection).backgroundColor;
+    navbar.style.backgroundColor = introSectionColor !== 'rgba(0, 0, 0, 0)' ? introSectionColor : fallbackColor;
   }
+}
+
+// Function to change the navbar color instantly based on the current theme and active section
+function changeNavbarColorInstantly() {
+  const navbar = document.querySelector('.nav-wrapper');
+  const theme = document.documentElement.getAttribute('data-theme');
+  const fallbackColor = theme === 'dark' ? darkColor : lightColor;
+  const currentSection = document.querySelector('.active-section');
+
+  if (currentSection) {
+    const sectionColor = getComputedStyle(currentSection).backgroundColor;
+    navbar.style.backgroundColor = sectionColor !== 'rgba(0, 0, 0, 0)' ? sectionColor : fallbackColor;
+  } else {
+    navbar.style.backgroundColor = fallbackColor;
+  }
+}
+
+// Function to handle theme change
+function handleThemeChange() {
+  const slider = document.getElementById('checkbox');
+  slider.addEventListener('change', function() {
+    if (slider.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+    changeNavbarColorInstantly();
+    updateNavbarColor();
+  });
+}
+
+// Function to set the initial theme based on the stored preference or browser's default preference
+function setInitialTheme() {
+  const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const slider = document.getElementById('checkbox');
+  const storedTheme = localStorage.getItem('theme');
+  const initialTheme = storedTheme || (prefersDarkMode ? 'dark' : 'light');
+  
+  // Set the initial theme on the document element
+  document.documentElement.setAttribute('data-theme', initialTheme);
+  
+  // Update the slider position based on the initial theme
+  if (initialTheme === 'dark') {
+    slider.checked = true;
+  }
+  
+  // Call the necessary functions to update the navbar color and handle theme change
+  changeNavbarColorInstantly();
+  updateNavbarColor();
+  handleThemeChange();
+}
+
+// Function to store the selected theme preference in local storage
+function storeThemePreference(theme) {
+  localStorage.setItem('theme', theme);
+}
+
+// Listen for the theme change event and update the navbar color
+document.addEventListener('themeChanged', function() {
+  changeNavbarColorInstantly();
+  window.requestAnimationFrame(updateNavbarColor);
 });
+
+// Call the setInitialTheme function on page load
+window.addEventListener('load', setInitialTheme);
+
+// Scroll event listener to update the navbar color while scrolling
+window.addEventListener('scroll', updateNavbarColor);
+
+// Call the handleThemeChange function to handle the theme change event
+handleThemeChange();
+
+// Function to handle theme change using the slider
+function handleSliderChange() {
+  const slider = document.getElementById('checkbox');
+  slider.addEventListener('change', function() {
+    if (slider.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      storeThemePreference('dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      storeThemePreference('light');
+    }
+    changeNavbarColorInstantly();
+    updateNavbarColor();
+  });
+}
+
+// Call the handleSliderChange function to handle the slider change event
+handleSliderChange();
 
 
 // Gmail Redirect Logic
@@ -107,31 +174,31 @@ downloadBtn.addEventListener('click', () => {
 });
 
 
+// Toggle button for Dark and light mode
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
-// Skills Slider 
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
 
-// Optional: Add functionality to the skill cloud
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
 
-// const skills = document.querySelectorAll('.skill');
+toggleSwitch.addEventListener('change', switchTheme, false);
 
-// skills.forEach(skill => {
-//   skill.addEventListener('click', () => {
-//     // Perform an action when a skill is clicked
-//     console.log(`Clicked on skill: ${skill.textContent}`);
-//   });
-// });
 
-// Footer Section
-// window.addEventListener('scroll', function() {
-//   var footer = document.querySelector('.footer');
-//   var scrollPosition = window.scrollY;
-
-//   if (scrollPosition > 0) {
-//     footer.style.backgroundColor = "#fff";
-//   } else {
-//     footer.style.backgroundColor = "#fdf6f8";
-//   }
-// });
 
 
 
